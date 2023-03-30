4f37d4f792b70970ce5bf347c84840048d3053bc
Draaft4 <draaft@outlook.es>
2023-03-30 09:22:27 -0500

    cambio a imagenes estaticas y base de login

--- lib/view/Login/LoginPage.dart
+++ lib/view/Login/LoginPage.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -11,8 +11,31 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Stack(children: [
+      background(),
+      backgroundFilter()
+    ],);
   }
 }
+
+BackdropFilter backgroundFilter() {
+    return BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.0),
+          ),
+        ),
+      );
+  }
+
+  Container background() {
+    return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                  "static/splash.jpeg"),
+              fit: BoxFit.cover),
+        ),
+      );
+  }
--- lib/view/Splash/SplashPage.dart
+++ lib/view/Splash/SplashPage.dart
@@ -26,55 +26,67 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: NetworkImage(
-                    "https://images.pexels.com/photos/3888048/pexels-photo-3888048.jpeg"),
-                fit: BoxFit.cover),
-          ),
-        ),
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.0),
-            ),
-          ),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-                child: SizedBox(
-                    width: 200.0,
-                    height: 100.0,
-                    child: Card(
-                      color: AppColors.backCard,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children:  [
-                            const SizedBox(height: 16.0),
-                            CircularProgressIndicator(color: AppColors.circularProgress),
-                          ],
-                        ),
-                        
-                      ),
-                    ))),
-          ],
-        ),
+        background(),
+        backgroundFilter(),
+        content(),
       ]),
     );
   }
 
+  Column content() {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: SizedBox(
+                  width: 200.0,
+                  height: 100.0,
+                  child: Card(
+                    color: AppColors.backCard,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children:  [
+                          const SizedBox(height: 16.0),
+                          CircularProgressIndicator(color: AppColors.circularProgress),
+                        ],
+                      ),
+                      
+                    ),
+                  ))),
+        ],
+      );
+  }
+
+  BackdropFilter backgroundFilter() {
+    return BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.0),
+          ),
+        ),
+      );
+  }
+
+  Container background() {
+    return Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                  "static/splash.jpeg"),
+              fit: BoxFit.cover),
+        ),
+      );
+  }
+
   cargarDatos() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 5));
     setState(() {
       cargando = !cargando;
     });
-    //navegar();
+    navegar();
   }
 
   navegar() async {
